b2d87d2fe11e24eda08d41ebf875d91c2b6a60a9
Joe <joe.ekiert@gmail.com>
2015-09-01 04:27:38 +0000

    removed deprecated methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -121,18 +121,6 @@
     return $element;
   };
 
-  // JOE: deprecate this shit
-  var buildHTML5VideoWith = function ($element) {
-    var $video = $('<video>', {
-          src: $element.data('src'),
-          height: 450,
-          width: 600,
-          controls: true
-        });
-
-    handleClick($element, $video);
-  };
-
   var buildIMGwith = function ($element) {
     var $img = $('<img>', {
           src: $element.dat('src'),
@@ -176,73 +164,48 @@
     });
   }
 
+  // Arctic Scroll Source
+  var setArcticScroll = function () {
+    var $postContent = $(".post-content");
+    $postContent.fitVids();
 
-  // deprecating this - currently not used
-  var buildiFrameWith = function ($element) {
-    var $iframe = $('<iframe>', {
-          src: $element.data('src'),
-          frameborder: 0,
-          height: 450,
-          width: 600,
-          scrolling: 'no'
-        });
+    $(".scroll-down").arctic_scroll();
 
-    $iframe.attr('allowfullscreen');
-
-    $element.on('click', function (e) {
+    $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
       e.preventDefault();
-
-      if ($iframe.is(':visible')) {
-        $iframe.detach();
-      }
-      else {
-        $iframe.appendTo($element);
-      }
+      $("body").toggleClass("nav-opened nav-closed");
     });
   };
+  // Arctic Scroll by Paul Adam Davis
+  // https://github.com/PaulAdamDavis/Arctic-Scroll
+  $.fn.arctic_scroll = function (options) {
 
-    // Arctic Scroll Source
-    var setArcticScroll = function () {
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        $(".scroll-down").arctic_scroll();
-
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
-    };
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+    var defaults = {
+      elem: $(this),
+      speed: 500
+    },
 
         allOptions = $.extend(defaults, options);
 
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
+    allOptions.elem.click(function (event) {
+      event.preventDefault();
+      var $this = $(this),
+          $htmlBody = $('html, body'),
+          offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
+          position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
+          toMove;
 
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
+      if (offset) {
+        toMove = parseInt(offset);
+        $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+      } else if (position) {
+        toMove = parseInt(position);
+        $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+      } else {
+        $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+      }
+    });
 
-    };
+  };
 
 })(jQuery);
